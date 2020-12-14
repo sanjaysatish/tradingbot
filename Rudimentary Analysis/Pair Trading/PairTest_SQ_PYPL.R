bf529efa660af4bac340dedb2607c285da897ebc
Sanjay Satish <sanjay.satish@duke.edu>
2020-12-14 09:37:38 -0800

    Added RScript and Data for pair test

---
+++ Rudimentary Analysis/Pair Trading/PairTest_SQ_PYPL.R
@@ -0,0 +1,9 @@
+# Script for testing pair trading strategy between SQ, PYPL 
+# Author: Sanjay Satish @sanjaysatish
+# Date Created: 12/14/2020
+
+# Loading libraries 
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(quantstrat)
